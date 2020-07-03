a0bba308d1957e59ad9557c7e658498f9c06c936
Jakub Staroń <kubastaron@hotmail.com>
2020-07-03 03:03:52 +0200

    windows without cygwin is gay

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,4 @@
-mkdir ../out
-mkdir ../out/bin
-mkdir ../out/Mods
-msbuild ../SixModLoader.sln /p:Configuration=Release
+mkdir ..\out
+mkdir ..\out\bin
+mkdir ..\out\Mods
+msbuild ..\SixModLoader.sln /p:Configuration=Release
