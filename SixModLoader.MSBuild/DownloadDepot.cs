12f3757b4cb6ee891ecd13698ae833fd6d58c71c
js6pak <kubastaron@hotmail.com>
2020-08-31 19:48:10 +0200

    windows... eh

--- SixModLoader.MSBuild/DownloadDepot.cs
+++ SixModLoader.MSBuild/DownloadDepot.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using DepotDownloader;
 using Microsoft.Build.Framework;
@@ -14,7 +15,6 @@ namespace SixModLoader.MSBuild
         public uint DepotId { get; set; } = ContentDownloader.INVALID_DEPOT_ID;
         public ulong ManifestId { get; set; } = ContentDownloader.INVALID_MANIFEST_ID;
         public string Branch { get; set; } = "public";
-        public string OperatingSystem { get; set; } = "linux";
 
         public override bool Execute()
         {
@@ -33,19 +33,52 @@ namespace SixModLoader.MSBuild
             ContentDownloader.Config.InstallDirectory = InstallDirectory;
             ContentDownloader.Config.UsingFileList = true;
             ContentDownloader.Config.FilesToDownloadRegex = new List<Regex>();
-            ContentDownloader.Config.FilesToDownload = new List<string>
+            ContentDownloader.Config.FilesToDownload = new List<string>();
+
+            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            foreach (var fileEntry in new[]
             {
                 "SCPSL_Data/Managed/Assembly-CSharp.dll",
-                "SCPSL_Data/Managed/Assembly-CSharp-firstpass.dll",
+                "SCPSL_Dzata/Managed/Assembly-CSharp-firstpass.dll",
                 "SCPSL_Data/Managed/CommandSystem.Core.dll",
 
                 "SCPSL_Data/Managed/Mirror.dll",
                 "SCPSL_Data/Managed/UnityEngine.dll",
                 "SCPSL_Data/Managed/UnityEngine.CoreModule.dll",
                 "SCPSL_Data/Managed/UnityEngine.PhysicsModule.dll"
-            };
+            })
+            {
+                try
+                {
+                    string fileEntryProcessed;
+                    if (isWindows)
+                    {
+                        // On Windows, ensure that forward slashes can match either forward or backslashes in depot paths
+                        fileEntryProcessed = fileEntry.Replace("/", "[\\\\|/]");
+                    }
+                    else
+                    {
+                        // On other systems, treat / normally
+                        fileEntryProcessed = fileEntry;
+                    }
 
-            ContentDownloader.DownloadAppAsync(AppId, DepotId, ManifestId, Branch, OperatingSystem, null, null, false, false).GetAwaiter().GetResult();
+                    var rgx = new Regex(fileEntryProcessed, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    ContentDownloader.Config.FilesToDownloadRegex.Add(rgx);
+                }
+                catch
+                {
+                    // For anything that can't be processed as a Regex, allow both forward and backward slashes to match
+                    // on Windows
+                    if (isWindows)
+                    {
+                        ContentDownloader.Config.FilesToDownload.Add(fileEntry.Replace("/", "\\"));
+                    }
+
+                    ContentDownloader.Config.FilesToDownload.Add(fileEntry);
+                }
+            }
+
+            ContentDownloader.DownloadAppAsync(AppId, DepotId, ManifestId, Branch, "linux", "64", null, false, false).GetAwaiter().GetResult();
 
             return true;
         }
